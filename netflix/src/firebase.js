d4ca5d598ff790a46b4137b3032baccf729550d4
asadiqbalkhan <iq.asad@gmail.com>
2021-07-12 00:07:00 +0500

    imdb api , nav bar completed, banner almost completed

---
+++ netflix/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBS9PQCR3nhXrRPFjM59Rjq5bOvSO13uRM",
+  authDomain: "netflix-clone-reactjs-72b7c.firebaseapp.com",
+  projectId: "netflix-clone-reactjs-72b7c",
+  storageBucket: "netflix-clone-reactjs-72b7c.appspot.com",
+  messagingSenderId: "45250811161",
+  appId: "1:45250811161:web:258e456899d4e9ab2a6188",
+};
