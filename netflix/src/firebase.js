714957bae84e2b13d3a93c7066853ff6a666d36f
asadiqbalkhan <iq.asad@gmail.com>
2021-07-13 05:25:40 +0500

    added components for homescreen, login functionality with firebase

--- netflix/src/firebase.js
+++ netflix/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBS9PQCR3nhXrRPFjM59Rjq5bOvSO13uRM",
   authDomain: "netflix-clone-reactjs-72b7c.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "45250811161",
   appId: "1:45250811161:web:258e456899d4e9ab2a6188",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
